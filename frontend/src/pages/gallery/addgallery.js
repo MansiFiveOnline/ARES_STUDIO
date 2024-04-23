912b4f12e8402741dd36238203b300d2e90e9767
AdarshFiveonline <167640932+MansiFiveOnline@users.noreply.github.com>
2024-04-23 12:09:30 +0530

    added pages

--- frontend/src/pages/gallery/addgallery.js
+++ frontend/src/pages/gallery/addgallery.js
@@ -2,7 +2,73 @@ import React from "react";
 import Layout from "../../components/layout";
 
 const AddGallery = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div className="theme-form-header">
+        <h2>Add Gallery</h2>
+      </div>
+      <div className="form-white-bg">
+        <form>
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+              <div className="theme-form">
+                <label>Service</label>
+                <input
+                  type="text"
+                  name="service"
+                  //   value={Title}
+                  // onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+              <div className="theme-form">
+                <label>Gallery Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  // value={Qualification}
+                  // onChange={(e) => setImage(e.target.files[0])}
+                  required
+                />
+                {/* <img className="form-profile" src="src/img/user-icon-img.png" /> */}
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+              <div className="theme-form">
+                <label>Media</label>
+                <input
+                  type="file"
+                  name="media"
+                  // value={Qualification}
+                  // onChange={(e) => setImage(e.target.files[0])}
+                />
+                <span> OR </span>
+
+                <input
+                  type="text"
+                  name="media"
+                  // value={Qualification}
+                  // onChange={(e) => setImage(e.target.files[0])}
+                />
+                {/* <img className="form-profile" src="src/img/user-icon-img.png" /> */}
+              </div>
+            </div>
+
+            <div className="col-12">
+              <div className="theme-form">
+                {/* <input type="button" value="Save" onClick={handleSubmit}/> */}
+                <button type="submit">Save</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default AddGallery;
--- frontend/src/pages/gallery/gallery.js
+++ frontend/src/pages/gallery/gallery.js
@@ -1,8 +1,111 @@
 import React from "react";
 import Layout from "../../components/layout";
+import { NavLink, Link } from "react-router-dom";
 
 const Gallery = () => {
-  return <Layout>Gallery</Layout>;
+  return (
+    <Layout>
+      <div className="pages-headers ">
+        <h2>
+          Gallery
+          <NavLink to="/add/gallery" className="theme-cta">
+            <i class="las la-plus-circle"></i>
+            Add Gallery
+          </NavLink>
+        </h2>
+      </div>
+      <div className="row mobilerows">
+        <div className="col-md-12">
+          <div className="infos-table">
+            <div className="table-responsive">
+              {/* <DataTable
+        value={users}
+        paginator
+        sortMode="multiple"
+        rows={1}
+        rowsPerPageOptions={[5, 10, 25, 50]}
+      >
+        <Column
+          field="name"
+          header="Name"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="designation"
+          header="Designation"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="linkedin_url"
+          header="LinkedIn URL"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="users.imageUrl"
+          header="Image"
+          sortable
+          style={{ width: "25%" }}
+        >
+          {" "}
+          <img
+            src=""
+            alt="new"
+            style={{ width: "50px", height: "50px" }}
+            header="Image"
+          />
+        </Column> */}
+
+              <table id="example" className="table nowrap">
+                <thead>
+                  <tr>
+                    <th>Service</th>
+                    <th className="text-center">Gallery Name</th>
+                    <th className="text-center">Media</th>
+                    <th className="text-center">Edit</th>
+                    <th className="text-center">Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* {users &&
+                  users.map((user) => ( */}
+                  <tr>
+                    <td>Games</td>
+                    <td className="text-center">GTA</td>
+                    <td className="table-profile-img text-center">
+                      <img
+                        src=""
+                        alt=""
+                        style={{ width: "50px", height: "50px" }}
+                      />
+                    </td>
+                    <td className="text-center">
+                      {/* <button title="Edit" onClick={() => navigate(`/edit/team/${user._id}`)}>
+                  <CreateIcon />
+                </button>  */}
+                      <Link to={`/edit/gallery/`} title="Edit">
+                        <i class="las la-pencil-alt"></i>
+                      </Link>
+                    </td>
+                    <td className="text-center">
+                      <button
+                        className="delete-btn"
+                        // onClick={() => handleDelete(user._id)}
+                      >
+                        <i class="las la-trash"></i>{" "}
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Gallery;
