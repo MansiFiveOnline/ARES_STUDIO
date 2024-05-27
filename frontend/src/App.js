b4db09c6f496a6dfba3b4e4c06d1dbf9d351e234
AdarshFiveonline <167640932+MansiFiveOnline@users.noreply.github.com>
2024-05-27 17:08:21 +0530

    added new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ import Servicedetail from "./pages/user/Servicedetail";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ares_studio">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
